a6db7a35fb9ef3f9470b14eeaa346ba91a48390d
Rahul Mitra <89061465+mrahul2001@users.noreply.github.com>
2026-05-20 09:21:11 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return Math.min(func(n, cost, dp), func(n, cost, dp));
+    }
+
+    private int func(int i, int[] cost, int[] dp) {
+        if (i == 0 || i == 1)
+            return 0;
+        
+        if (dp[i] != -1)
+            return dp[i];
+
+        int step1 = func(i - 1, cost, dp) + cost[i - 1];
+        int step2 = func(i - 2, cost, dp) + cost[i - 2];
+
+        return dp[i] = Math.min(step1, step2);
+    }
+}
