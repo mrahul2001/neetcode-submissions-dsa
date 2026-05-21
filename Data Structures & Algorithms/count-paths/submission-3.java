b9f9d423c99d3059e2d00c10e328b88db3a95cb1
Rahul Mitra <89061465+mrahul2001@users.noreply.github.com>
2026-05-21 08:59:44 +0530

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] prev = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            int[] temp = new int[n];
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    temp[j] = 1;
+                    continue;
+                }
+
+                int up = 0, left = 0;
+                if (i > 0)
+                    up += prev[j];
+                if (j > 0)
+                    left += temp[j - 1];
+
+                temp[j] = up + left;
+            }
+            prev = temp;
+        }
+
+        return prev[n - 1];
+    }
+
+    private int func(int i, int j, int[][] dp) {
+        if (i < 0 || j < 0)
+            return 0;
+
+        if (i == 0 && j == 0)
+            return 1;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int up = func(i - 1, j, dp);
+        int left = func(i, j - 1, dp);
+
+        return dp[i][j] = up + left;
+    }
+}
