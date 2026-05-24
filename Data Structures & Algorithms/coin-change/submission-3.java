4641d55851426d7132a5968b55cf34d19483e4de
Rahul Mitra <89061465+mrahul2001@users.noreply.github.com>
2026-05-24 09:49:00 +0530

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+
+        int[][] dp = new int[n][amount + 1];
+        for (int amt = 0; amt <= amount; amt++) {
+            if (amt % coins[0] == 0)
+                dp[0][amt] = amt / coins[0];
+            else
+                dp[0][amt] = (int) 1e9;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int amt = 0; amt <= amount; amt++) {
+                int take = (int) 1e9;
+                if (coins[i] <= amt) {
+                    take = 1 + dp[i][amt - coins[i]];
+                }
+                int notTake = dp[i - 1][amt];
+
+                dp[i][amt] = Math.min(take, notTake);
+            }
+        }
+
+        int ans = dp[n - 1][amount];
+        if (ans >= 1e9)
+            return -1;
+        return ans;
+    }
+
+    private int func(int i, int[] coins, int amt, int[][] dp) {
+        if (i == 0) {
+            if (amt % coins[0] == 0)
+                return amt / coins[0];
+
+            return (int) 1e9;
+        }
+
+        if (dp[i][amt] != -1)
+            return dp[i][amt];
+
+        int take = (int) 1e9;
+        if (coins[i] <= amt) {
+            take = 1 + func(i, coins, amt - coins[i], dp);
+        }
+        int notTake = func(i - 1, coins, amt, dp);
+
+        return dp[i][amt] = Math.min(take, notTake);
+    }
+}
