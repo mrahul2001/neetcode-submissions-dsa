701feca598c3751c35e2b4264fdf87776869d14c
Rahul Mitra <89061465+mrahul2001@users.noreply.github.com>
2026-06-04 09:11:14 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int lastGoodPosition = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] + i >= lastGoodPosition)
+                lastGoodPosition = i;
+        }
+
+        return lastGoodPosition == 0;
+    }
+}
