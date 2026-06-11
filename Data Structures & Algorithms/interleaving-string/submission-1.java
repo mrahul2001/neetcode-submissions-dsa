b500c4a0edf9cc4b2b334dc6f76af257d4d98856
Rahul Mitra <89061465+mrahul2001@users.noreply.github.com>
2026-06-12 00:40:42 +0530

    Add: interleaving-string - submission-1

---
+++ Data Structures & Algorithms/interleaving-string/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length())
+            return false;
+
+       return func(0, 0, 0, s1, s2, s3); 
+    }
+
+    private boolean func(int i, int j, int k, String s1, String s2, String s3) {
+        if (i == s1.length() && j == s2.length() && k == s3.length())
+            return true;
+        
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
+            if (func(i + 1, j, k + 1, s1, s2, s3))
+                return true;
+        }
+
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
+            if (func(i, j + 1, k + 1, s1, s2, s3))
+                return true;
+        }
+        
+        return false;
+    }
+}
