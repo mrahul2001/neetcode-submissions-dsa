68b098ce101295c0943a7a4ae4c8aec330e23f49
Rahul Mitra <89061465+mrahul2001@users.noreply.github.com>
2026-06-12 16:15:16 +0530

    Add: stone-game - submission-5

---
+++ Data Structures & Algorithms/stone-game/submission-5.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+
+        int total = 0;
+        for (int pile : piles) total += pile;
+
+        int[][] dp = new int[n][n];
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                boolean even = (j - i) % 2 == 0;
+
+                int left = even ? piles[i] : 0;
+                int right = even ? piles[j] : 0;
+
+                if (i == j) {
+                    dp[i][j] = piles[i];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j] + left, dp[i][j - 1] + right);
+                }
+            }
+        }
+
+        int aliceScore = dp[0][n - 1];
+        int bobScore = total - aliceScore;
+
+        return aliceScore > bobScore;
+    }
+
+    private int func(int i, int j, int[] piles, int[][] dp) {
+        if (i > j)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        boolean even = (j - i) % 2 == 0;
+
+        int left = even ? piles[i] : 0;
+        int right = even ? piles[j] : 0;
+
+        return dp[i][j] =
+                   Math.max(func(i + 1, j, piles, dp) + left, func(i, j - 1, piles, dp) + right);
+    }
+}
